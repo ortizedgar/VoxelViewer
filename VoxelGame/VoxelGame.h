4e19084b86f107a7473b5270d7bc719ab24093d9
mbanquiero <mbanquiero@gmail.com>
2016-09-17 18:09:40 -0300

    Add project files.

---
+++ VoxelGame/VoxelGame.h
@@ -0,0 +1,35 @@
+
+// VoxelGame.h : main header file for the VoxelGame application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CVoxelGameApp:
+// See VoxelGame.cpp for the implementation of this class
+//
+
+class CVoxelGameApp : public CWinApp
+{
+public:
+	CVoxelGameApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CVoxelGameApp theApp;
